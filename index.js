f831fc45720953ded4c5aeeda95402212326e0ba
asta345 <81383112+asta345@users.noreply.github.com>
2021-09-09 00:32:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 9000;
+ const port = 9000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 // used for session cookie
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+//to connect mongo store 
+ const MongoStore = require('connect-mongo')(session);
 
 app.use(express.urlencoded());
 
@@ -27,7 +29,7 @@ app.set('layout extractScripts', true);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
+// mongo store is used to store the session cookie in the db
 app.use(session({
     name: 'codeial',
     // TODO change the secret before deployment in production mode
@@ -36,9 +38,19 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
-    }
+    },
+    store: new  MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        
+        },
+        function(err){
+            console.log(err ||  'connect-mongodb setup ok');
+        }
+    )
 }));
-
+    
 app.use(passport.initialize());
 app.use(passport.session());
 
