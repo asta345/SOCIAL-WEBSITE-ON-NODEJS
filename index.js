2526365a7de84ba8d14fc53606200f0f17e0b850
asta345 <81383112+asta345@users.noreply.github.com>
2021-09-11 00:42:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 8000;
+const port = 9000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 // used for session cookie
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
-const MongoStore = require('connect-mongo')(session);
+// const MongoStore = require('connect-mongo')(session);
+const sassMiddleware =require('node-sass-middleware');
 
+app.use(sassMiddleware({
+   src:'./assets/scss',
+   dest:'./assets/css',
+    debug: true,
+    outputStyle:'extended',
+    prefix:'/css'
+
+}));
 app.use(express.urlencoded());
-
 app.use(cookieParser());
 
 app.use(express.static('./assets'));
@@ -38,16 +46,16 @@ app.use(session({
     cookie: {
         maxAge: (1000 * 60 * 100)
     },
-    store: new MongoStore(
-        {
-            mongooseConnection: db,
-            autoRemove: 'disabled'
+    // store: new MongoStore(
+    //     {
+    //         mongooseConnection: db,
+    //         autoRemove: 'disabled'
         
-        },
-        function(err){
-            console.log(err ||  'connect-mongodb setup ok');
-        }
-    )
+    //     },
+    //     function(err){
+    //         console.log(err ||  'connect-mongodb setup ok');
+    //     }
+    // )
 }));
 
 app.use(passport.initialize());
