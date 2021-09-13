b86521a56cd7c10ed99570d366e0acc2f4f12080
asta345 <81383112+asta345@users.noreply.github.com>
2021-09-13 16:36:19 +0530

    Add files via upload

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,14 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
     console.log(req.cookies);
     res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
+    Post.find({},function (err,posts){
+        return res.render('home',{
+        title:"Codeial| Home",
+        posts: posts
+    });
+   
     });
 }
 
